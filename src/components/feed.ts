0ade20e295a36ecc01961ae94fedc1922f29f37f
Matheus Victor <matheusvictor2000@gmail.com>
2022-10-29 12:39:40 -0300

    added function post cards

--- src/components/feed.ts
+++ src/components/feed.ts
@@ -12,7 +12,7 @@ export function feed(): string {
                 <div class="form-area">
                 <img src="${profileImg}" alt="Profile" class="feed-img">
                 <textarea name="Tweet" id="tweet" class="tweet-area" 
-                placeholder="What’s happening?"  rows="2" cols="30" maxlength="380"></textarea>
+                placeholder="What’s happening?"  rows="2" cols="30" maxlength="380" required></textarea>
                 </div>
                 <div class="feed__post">
                     <div class="feed__post-icons">
@@ -27,41 +27,6 @@ export function feed(): string {
                 </div>
             </form>
             <div id="view">
-            <div class="tweet__card">
-            <div class="tweet__header">
-                <img src="${profileImg}" alt="" class="feed-img">
-                <div class="tweet__header-info">
-                    <span>Matheus Victor</span> <span class="tweet__header-id">@mvictor</span><span class="tweet__header-id">. Jun 27</span>
-                <p class="tweet__descrpt">If you're tired of ola!</p>
-                
-            </div>
-            </div>
-            <div class="tweet__header-img">
-                <img src="https://pbs.twimg.com/media/Dgti2h0WkAEUPmT.png" alt="Tweet Img" class="tweet-img">
-            </div>
-            <div class="tweet__info-counts">
-                <div class="tweet__info-content">
-                <i class="fa-regular fa-comment"></i>
-                <div class="count">33</div>
-            </div>
-            <div class="tweet__info-content">
-                <i class="fa-solid fa-retweet"></i>
-                <div class="count">397</div>
-            </div>
-            <div class="tweet__info-content">
-                <i class="fa-regular fa-heart"></i>
-                <div class="count">
-                2.6k
-                </div>
-            </div>
-            <div class="tweet__info-content">
-                <i class="fa-regular fa-folder"></i>
-                <div class="count">
-                2.6k
-                </div>
-            </div>
-            </div>
-            </div>
             </div>
         </div>  
     </div>
--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 const buttonFocus = document.querySelector<HTMLButtonElement>('.menu__side-btn')!; // BUTTON SIDEBAR MENU LEFT
 const textArea = document.querySelector<HTMLInputElement>('#tweet')!; // INPUT TEXTAREA
-const fileArea: any = document.querySelector<HTMLInputElement>('#file-image')!; // INPUT FILE
+const fileArea: HTMLInputElement | any = document.querySelector('#file-image'); // INPUT FILE
 const form = document.querySelector<HTMLElement>('#form-tweet')!;
 const viewFeed = document.querySelector<HTMLDivElement>('#view')!;
 
@@ -26,11 +26,76 @@ const viewFeed = document.querySelector<HTMLDivElement>('#view')!;
 
 const CLIENT_ID: string = '4ce2a6c0ddba9b6';
 
+interface post {
+    text: string,
+    date: Date,
+    url: string
+}
+
+let postContent: post[] = [];
+
 // FUNCTIONS
 
-function view(text: string, date: Date, url: string) {
+function save(text: string, date: Date, url: string) {
+    postContent.push({text, date, url})
     
+    view(postContent);
+
+    update()
 }
+
+function update() {
+    let teste = view(postContent)
+    
+    viewFeed.innerHTML = teste;
+}
+
+
+
+function view(postContent: post[]) {
+    return postContent.map(item => {
+        return  `
+        <div class="tweet__card">
+        <div class="tweet__header">
+            <img src="/src/img/profile-img.png" alt="" class="feed-img">
+            <div class="tweet__header-info">
+                <span>Matheus Victor</span> <span class="tweet__header-id">@mvictor</span><span class="tweet__header-id"> ${item.date}</span>
+            <p class="tweet__descrpt">${item.text}</p>
+            
+        </div>
+        </div>
+        <div class="tweet__header-img">
+            <img src="${item.url}" alt="Tweet Img" class="tweet-img">
+        </div>
+        <div class="tweet__info-counts">
+            <div class="tweet__info-content">
+            <i class="fa-regular fa-comment"></i>
+            <div class="count">33</div>
+        </div>
+        <div class="tweet__info-content">
+            <i class="fa-solid fa-retweet"></i>
+            <div class="count">397</div>
+        </div>
+        <div class="tweet__info-content">
+            <i class="fa-regular fa-heart"></i>
+            <div class="count">
+            2.6k
+            </div>
+        </div>
+        <div class="tweet__info-content">
+            <i class="fa-regular fa-folder"></i>
+            <div class="count">
+            2.6k
+            </div>
+        </div>
+        </div>
+        </div>
+        `
+    }).join('')
+
+
+}
+
 
 function clearForm() { // CLEAR FORM
     textArea.value = '';
@@ -56,8 +121,9 @@ form.addEventListener('submit', (e) => { // SUBMIT FORM
         }
     })
         .then(data => data.json())
-        .then(data => view(textAreaValue, datePost,data.data.link)); // PASSING PARAMETERS FOR FUNCTION VIEW
+        .then(data => save(textAreaValue, datePost,data.data.link)); // PASSING PARAMETERS FOR FUNCTION VIEW
         clearForm(); // CLEAR FORM
+        
 })
 
 buttonFocus.addEventListener('click', () => { // BUTTON TWEET IN SIDE MENU LEFT TO FOCUS IN INPUT TEXT AREA
